7c31271c1b381fbd30f85ade84e01bff8a5252bb
Patricia <lopezpa@unican.es>
2022-09-27 13:50:20 +0200

    Cambiar tests iniciales

--- AndroidProject/app/src/androidTest/java/es/unican/is/appgasolineras/activities/main/MostrarGasolinerasUITest.java
+++ AndroidProject/app/src/androidTest/java/es/unican/is/appgasolineras/activities/main/MostrarGasolinerasUITest.java
@@ -1,12 +1,11 @@
 package es.unican.is.appgasolineras.activities.main;
 
-import static androidx.test.espresso.Espresso.onData;
-import static androidx.test.espresso.action.ViewActions.click;
+
+import static androidx.test.espresso.Espresso.onView;
+import static androidx.test.espresso.assertion.ViewAssertions.matches;
 import static androidx.test.espresso.matcher.ViewMatchers.withId;
+import static es.unican.is.appgasolineras.utils.Matchers.hasElements;
 
-import static org.hamcrest.Matchers.anything;
-
-import androidx.test.espresso.matcher.ViewMatchers;
 import androidx.test.ext.junit.rules.ActivityScenarioRule;
 
 import org.junit.AfterClass;
@@ -17,7 +16,7 @@ import org.junit.Test;
 import es.unican.is.appgasolineras.R;
 import es.unican.is.appgasolineras.repository.rest.GasolinerasServiceConstants;
 
-public class MainViewUITest {
+public class MostrarGasolinerasUITest {
 
     @BeforeClass
     public static void setUp() {
@@ -35,7 +34,7 @@ public class MainViewUITest {
 
     @Test
     public void openDetailViewTest() {
-        onData(anything()).inAdapterView(ViewMatchers.withId(R.id.lvGasolineras)).atPosition(0).perform(click());
+        onView(withId(R.id.lvGasolineras)).check(matches(hasElements()));
     }
 
 }
