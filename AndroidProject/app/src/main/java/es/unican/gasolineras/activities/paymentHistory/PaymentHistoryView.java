8be1b2407996e8c9d06482e2b3c807eb07c9f482
Alejandro8950 <alejandroacebo6@gmail.com>
2024-10-17 11:02:33 +0200

    Cherrypicking de la base de datos aceptado

--- AndroidProject/app/src/main/java/es/unican/gasolineras/activities/paymentHistory/PaymentHistoryView.java
+++ AndroidProject/app/src/main/java/es/unican/gasolineras/activities/paymentHistory/PaymentHistoryView.java
@@ -1,51 +1,63 @@
 package es.unican.gasolineras.activities.paymentHistory;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.view.Menu;
-import android.view.MenuInflater;
-import android.view.MenuItem;
-import android.view.View;
+import android.widget.ListView;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.room.Room;
 
-import androidx.appcompat.widget.Toolbar;
+import java.util.List;
 
 import es.unican.gasolineras.R;
-import es.unican.gasolineras.activities.info.InfoView;
-import es.unican.gasolineras.activities.registerPayment.RegisterPaymentView;
+import es.unican.gasolineras.activities.main.GasolinerasArrayAdapter;
+import es.unican.gasolineras.activities.main.IMainContract;
+import es.unican.gasolineras.model.Gasolinera;
+import es.unican.gasolineras.model.Pago;
+import es.unican.gasolineras.repository.AppDatabase;
+import es.unican.gasolineras.repository.IPagoDAO;
 
-public class PaymentHistoryView extends AppCompatActivity {
+/**
+ * View
+ */
+public class PaymentHistoryView extends AppCompatActivity implements IPaymentHistoryContract.View{
+    //Cherry
+    private AppDatabase db;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_payment_history);
-
-        Toolbar toolbar = findViewById(R.id.toolbarRegister);
-        setSupportActionBar(toolbar);
-        getSupportActionBar().setTitle("Historial de pagos");
+        db = Room.databaseBuilder(getApplicationContext(),
+                AppDatabase.class, "database-name").build();
     }
 
+    /**
+     * No tiene uso ya que no vamos a usar un on click listener
+     */
     @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        MenuInflater menuInflater = getMenuInflater();
-        menuInflater.inflate(R.menu.menu_history, menu);
-        return true;
+    public void init() {
+
     }
 
+    /**
+     * @see IPaymentHistoryContract.View#getPagoDAO()
+     * @return IPagoDAO de la base de datos
+     */
     @Override
-    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
-        int itemId = item.getItemId();
-        if (itemId == R.id.menuItemAddPago) {
-            Intent intent = new Intent(this, RegisterPaymentView.class);
-            startActivity(intent);
-            return true;
-        } else if (itemId == R.id.menuItemInfo) {
-            Intent intent = new Intent(this, InfoView.class);
-            startActivity(intent);
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
+    public IPagoDAO getPagoDAO() {
+        return db.pagoDAO();
     }
+
+    /**
+     * @see IPaymentHistoryContract.View#showPagos(List)
+     * @param pagos la lista de registro de pagos
+     */
+    @Override
+    public void showPagos(List<Pago> pagos) {
+        ListView list = findViewById(R.id.lvPagos);
+        PagosArrayAdapter adapter = new PagosArrayAdapter(this, pagos);
+        list.setAdapter(adapter);
+    }
+
+
 }
---
+++ AndroidProject/app/src/main/java/es/unican/gasolineras/model/Pago.java
@@ -0,0 +1,36 @@
+package es.unican.gasolineras.model;
+
+import androidx.room.*;
+
+import java.time.LocalDate;
+
+import lombok.Getter;
+import lombok.Setter;
+
+@Entity
+@Getter
+@Setter
+public class Pago {
+    // Cherr
+    @PrimaryKey(autoGenerate = true)
+    public int pid;
+
+    @ColumnInfo(name = "station_name")
+    public String stationName;
+
+    @ColumnInfo(name = "date")
+    public LocalDate date;
+
+    @ColumnInfo(name= "fuel_type")
+    public String fuelType;
+
+    @ColumnInfo(name= "quantity")
+    public Double quantity;
+
+    @ColumnInfo(name= "final_price")
+    public Double finalPrice;
+
+    @ColumnInfo(name= "price_per_litre")
+    public Double pricePerLitre;
+
+}
