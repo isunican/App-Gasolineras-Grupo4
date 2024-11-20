6c9d52d376d2568520437296c214c6e0b7c6d89d
Jlh331 <jlh331@alumnos.unican.es>
2024-11-20 15:26:27 +0100

    Integrado test unitarios de onDeleteConfirmed en la clase HistoryPaymentPresenterTest

--- AndroidProject/app/src/test/java/es/unican/gasolineras/activities/paymentHistory/HistoryPaymentPresenterTest.java
+++ AndroidProject/app/src/test/java/es/unican/gasolineras/activities/paymentHistory/HistoryPaymentPresenterTest.java
@@ -45,6 +45,7 @@ public class HistoryPaymentPresenterTest {
     private PaymentHistoryPresenter sut;
 
     private Pago p = new Pago();
+    private Pago p2 = new Pago();
     private LinkedList<Pago> pagos, pagosVacio;
 
     @Before
@@ -60,12 +61,21 @@ public class HistoryPaymentPresenterTest {
         p.setStationName("Prueba");
         p.setFinalPrice(20.0);
 
+        // Example payment 2
+        p.stationName="Avia";
+        p.date= String.valueOf(LocalDate.now());
+        p.fuelType="Biodiesel";
+        p.quantity=20.0;
+        p.pricePerLitre=1.50;
+        p.finalPrice=30.0;
+
         // Initialize the lists of payments
         pagos = new LinkedList<Pago>();
         pagosVacio = new LinkedList<Pago>();
 
         // Add the payment to the list
         pagos.add(p);
+        pagos.add(p2);
 
         // Prepare the return of the mocks
         when(mockPaymentDAO.getAll()).thenReturn(pagos);
@@ -104,4 +114,35 @@ public class HistoryPaymentPresenterTest {
         verify(mockPaymentView3,times(1)).showErrorBD();
     }
 
+
+    /**
+     * Tests del metodo onDeleteConfirmedSuccess (Jesus lobos)
+     */
+    @Test
+    public void testOnDeleteConfirmedSuccess() {
+        // caso 1 eliminado con exito
+        sut.init(mockPaymentView);
+        sut.onDeleteConfirmed(p2);
+        verify(mockPaymentDAO, times(1)).delete(p2);
+        verify(mockPaymentView, times(1)).showAlertDialog("Éxito eliminación", "El pago ha sido eliminado de manera exitosa de la base de datos");
+    }
+
+
+    @Test
+    public void testOnDeleteConfirmedBDError() {
+        // caso 2 error BD
+        sut.init(mockPaymentView3);
+        sut.onDeleteConfirmed(p2);
+
+        verify(mockPaymentView3, times(1)).showErrorBD();
+    }
+    /**
+     *
+     *  @Test
+     *     public void testOnDeleteConfirmedNullPago() {
+     *         // caso 3 , el pago que recibe el metodo es nulo
+     *         este escenario es imposible y por lo tanto no se implementa
+     *
+     *     }
+     */
 }
